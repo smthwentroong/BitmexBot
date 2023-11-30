875683bfafe3c66060bb8917f76596190faf22ce
kkopienko <kirill.kopienko@gmail.com>
2023-11-30 18:49:03 +0300

    edited base urls for test and real environments

--- src/main/java/kirill/kopienko/bitmexbot/util/Endpoints.java
+++ src/main/java/kirill/kopienko/bitmexbot/util/Endpoints.java
@@ -1,6 +1,7 @@
 package kirill.kopienko.bitmexbot.util;
 
 public class Endpoints {
-    public static final String BASE_URL = "https://testnet.bitmex.com/api/v1";
+    public static final String BASE_TEST_URL = "https://testnet.bitmex.com/api/v1";
+    public static final String BASE_REAL_URL = "https://bitmex.com/api/v1";
     public static final String ORDER_ENDPOINT = "/order";
 }
