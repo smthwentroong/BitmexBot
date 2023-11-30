d1ba44351de0e347ceab6da244cb4a4bda5bfd04
kkopienko <kirill.kopienko@gmail.com>
2023-11-30 18:42:11 +0300

    created a BitmexClientFactory wrapper-class using factory pattern. the goal is to make real BitmexClient and TestnetBitmexClient.

---
+++ src/main/java/kirill/kopienko/bitmexbot/service/BitmexClientFactory.java
@@ -0,0 +1,11 @@
+package kirill.kopienko.bitmexbot.service;
+
+public class BitmexClientFactory {
+    public BitmexClient newTestnetBitmexClient() {
+        return null;
+    }
+
+    public BitmexClient newBitmexClient() {
+        return null;
+    }
+}
