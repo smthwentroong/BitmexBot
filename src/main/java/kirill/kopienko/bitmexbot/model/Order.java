0b1bbfcc172575a5e9929150f2c12c7ee0c956fa
kkopienko <kirill.kopienko@gmail.com>
2023-12-01 16:59:21 +0300

    filled order model

--- src/main/java/kirill/kopienko/bitmexbot/model/Order.java
+++ src/main/java/kirill/kopienko/bitmexbot/model/Order.java
@@ -1,4 +1,13 @@
 package kirill.kopienko.bitmexbot.model;
 
 public class Order {
+    private Symbol symbol;
+    private boolean isBuy;
+    private double orderQty;
+    private String orderId;
+    private Double price;
+    private Double stopPx;
+    private boolean isWorking;
+    private OrderType orderType;
+    private OrderStatus orderStatus;
 }
