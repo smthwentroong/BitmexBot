b690424f2b299f78d9267ef207b7ebe49ae7d4d0
kkopienko <kirill.kopienko@gmail.com>
2023-12-02 15:58:44 +0300

    created BitmexClient class with basic fields defining our working fields, api-signature, endpoints for http requests

--- src/main/java/kirill/kopienko/bitmexbot/service/BitmexClient.java
+++ src/main/java/kirill/kopienko/bitmexbot/service/BitmexClient.java
@@ -1,4 +1,21 @@
 package kirill.kopienko.bitmexbot.service;
 
+import kirill.kopienko.bitmexbot.util.Signature;
+import lombok.RequiredArgsConstructor;
+
+import java.net.http.HttpClient;
+@RequiredArgsConstructor
 public class BitmexClient {
+    private final HttpClient httpClient = HttpClient.newBuilder().build();
+    private final String baseUrl;
+    private final boolean isReal;
+    private final Signature signature = new Signature();
+
+    private void sendRequest() {
+
+    }
+
+    private void cancelOrder() {
+
+    }
 }
